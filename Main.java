add0d0916b8671c1a4c92839891ab9b7d3a8f34e
Luciano Delfino <luciano.dlf@gmail.com>
2022-11-15 00:31:17 -0300

    Commit 2

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy un proyecto en Java ameo!");
+		System.out.println("Esta coneccion con github esta completada ameo!!")
 	}
 
 }
