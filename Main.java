8d69a97a54b6ed154ba4aec95beb709715ba8185
Luciano Delfino <luciano.dlf@gmail.com>
2022-11-15 00:34:50 -0300

    Arregla fix

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main{
 	public static void main(String[] args) {
 		System.out.println("Hola, soy un proyecto en Java ameo!");
 		System.out.println("Esta coneccion con github esta completada ameo!!")
+		System.out.println("Arreglamos un fix!!")
 	}
 
 }
